84990ffc622c861e8ec3cd29e37d091731c8b797
Venkat Peri <venkatperi@gmail.com>
2021-03-27 10:26:01 -0400

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,64 @@
 import React, { useState } from "react";
-import { Alert, Button, Input, Select } from "antd";
+import { Alert, Button, Input } from "antd";
 import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import "./App.css";
 import { roman2unicode, UNICODE_BLOCKS } from "./utils/transliterate";
 
-import convert from "xml-js";
+import convert, { Element } from "xml-js";
 
 const { TextArea } = Input;
-const { Option } = Select;
+
+function trans(tag: Element, target: number): Element | undefined {
+  const el = tag.elements && tag.elements.length > 0 && tag.elements[0];
+  // @ts-ignore
+  const tel = roman2unicode(el?.text, target);
+
+  return {
+    type: "element",
+    name: `${tag.name}t`,
+    elements: [
+      {
+        type: "text",
+        text: tel,
+      },
+    ],
+  };
+}
+
+function addTags(roman: string, target: number): string {
+  const data = convert.xml2js(roman, { compact: false });
+  const items = data.elements[0].elements;
+
+  for (let item of items) {
+    const elements: Element[] = [];
+    for (let tag of item.elements) {
+      elements.push(tag);
+      if (["mw", "pl", "ob"].includes(tag.name) && tag.elements?.length > 0) {
+        const t = trans(tag, target);
+        if (t) elements.push(t);
+      } else if (tag.name === "mg") {
+        const el2: Element[] = [];
+        for (let tag2 of tag.elements) {
+          el2.push(tag2);
+          if (tag2.name === "tel") {
+            const t = trans(tag2, target);
+            if (t) el2.push(t);
+          }
+        }
+        tag.elements = el2;
+      }
+    }
+    item.elements = elements;
+  }
+  return convert.js2xml(data, { spaces: 2 });
+}
 
 const App = () => {
   const [roman, setRoman] = useState<string>("");
   const [error, setError] = useState<string>("");
   const [converted, setConverted] = useState<string>("");
-  const [target, setTarget] = useState(UNICODE_BLOCKS.telugu);
+  const [target] = useState(UNICODE_BLOCKS.telugu);
 
   return (
     <>
@@ -24,39 +68,15 @@ const App = () => {
             onClick={() => {
               setError("");
               try {
-                const data = convert.xml2js(roman, { compact: false });
-                const items = data.elements[0].elements;
-                for (let item of items) {
-                  for (let tag of item.elements) {
-                    if (
-                      ["mw", "pl", "ob"].includes(tag.name) &&
-                      tag.elements?.length > 0
-                    ) {
-                      item.elements.push({
-                        type: "element",
-                        name: `${tag.name}t`,
-                        elements: [
-                          {
-                            type: "text",
-                            text: roman2unicode(tag.elements[0].text, target),
-                          },
-                        ],
-                      });
-                    }
-                  }
-                }
-
-                console.log(items);
-                setConverted(convert.js2xml(data, { spaces: 2 }));
+                setConverted(addTags(roman, target));
               } catch (e) {
                 setError(e.message);
               }
-              // setConverted(roman2unicode(roman, target));
             }}
           >
             Convert
           </Button>
-          {error.length && <Alert type="error" message={error} />}
+          {error.length > 0 && <Alert type="error" message={error} />}
         </Col>
       </Row>
       <Row className="height100">
